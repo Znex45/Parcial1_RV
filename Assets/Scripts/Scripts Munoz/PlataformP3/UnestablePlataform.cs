262b413a101e31845780839846d771ebcd21514e
Znex45 <anfeval30@gmail.com>
2026-03-07 08:08:50 -0500

    modificaciones

--- Assets/Scripts/Scripts Munoz/PlataformP3/UnestablePlataform.cs
+++ Assets/Scripts/Scripts Munoz/PlataformP3/UnestablePlataform.cs
@@ -4,9 +4,12 @@ public class UnstablePlatform : MonoBehaviour
 {
     public float moveAmount = 0.5f;
     public float speed = 2f;
+    public float recoveryDelay = 2f;
 
     private Vector3 startPos;
     private bool isStabilized = false;
+    private float cooldownTimer = 0f;
+    private float internalTime = 0f;
 
     void Start()
     {
@@ -15,15 +18,26 @@ public class UnstablePlatform : MonoBehaviour
 
     void Update()
     {
-        if (!isStabilized)
+
+        if (isStabilized)
         {
-            float offset = Mathf.Sin(Time.time * speed) * moveAmount;
-            transform.position = startPos + new Vector3(offset, 0, 0);
+            cooldownTimer = recoveryDelay;
+            return;
         }
+
+        if (cooldownTimer > 0)
+        {
+            cooldownTimer -= Time.deltaTime;
+            return;
+        }
+        internalTime += Time.deltaTime;
+        float offset = Mathf.Sin(internalTime * speed) * moveAmount;
+        transform.position = startPos + new Vector3(offset, 0, 0);
     }
 
     public void SetStabilized(bool state)
     {
         isStabilized = state;
+
     }
 }
